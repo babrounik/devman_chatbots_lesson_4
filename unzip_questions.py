e4d875d07289315d6ec64e57bb18863db208565f
Artsiom Babrounik <artsiom.babrounik>
2023-01-17 09:23:06 +0400

    Extracts questions from files.

---
+++ unzip_questions.py
@@ -0,0 +1,20 @@
+import glob
+import json
+
+questions_files = glob.glob("./quiz-questions/*.txt")
+
+questions = {}
+
+for question_path in questions_files:
+    with open(question_path, encoding="KOI8-R") as file:
+        file_content = file.read()
+        file_content_split = file_content.split("\n\n")
+
+        for line_index, line in enumerate(file_content_split):
+            key = line.split("\n")[0]
+            if key.startswith("Вопрос"):
+                question = "\n".join(file_content_split[line_index].split("\n")[1:])
+                answer = "\n".join(file_content_split[line_index + 1].split("\n")[1:])
+                questions[question] = answer
+
+print(questions)
