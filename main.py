b2590e386f3c984baa9c1b4c15073667fe29838b
Artsiom Babrounik <artsiom.babrounik>
2023-02-05 20:28:46 +0400

    Bot saves question to Redis.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import glob
+import random
 from functools import partial
 from dotenv import load_dotenv
 
+import redis
 from unzip_questions import load_questions
 from pathlib import Path
 from telegram import Update, ForceReply, ReplyKeyboardMarkup
@@ -18,14 +20,29 @@ def start(update: Update, context: CallbackContext) -> None:
     )
 
 
-def reply(update: Update, context: CallbackContext, questions) -> None:
-    custom_keyboard = [["Новый вопрос", 'top-right'],
-                       ['bottom-left', 'bottom-right']]
+def reply(update: Update, context: CallbackContext, questions, redis_conn) -> None:
+    custom_keyboard = [["Новый вопрос", 'Показать текущий вопрос'],
+                       ['Показать ответ', 'bottom-right']]
 
     text_response = "buttons"
     if update.message.text == "Новый вопрос":
-        for question, answer in questions.items():
-            text_response = question
+        question, _ = random.choice(list(questions.items()))
+        text_response = question
+        redis_conn.set(update.effective_user.id, question)
+    elif update.message.text == 'Показать текущий вопрос':
+        question = redis_conn.get(update.effective_user.id).decode("utf-8")
+        text_response = question
+    elif update.message.text == 'Показать ответ':
+        question = redis_conn.get(update.effective_user.id).decode("utf-8")
+        answer = questions.get(question).replace('"', '')
+        text_response = answer
+    else:
+        question = redis_conn.get(update.effective_user.id).decode("utf-8")
+        answer = questions.get(question).replace('"', '')
+        if update.message.text == answer:
+            text_response = "Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»"
+        else:
+            text_response = "Неправильно… Попробуешь ещё раз?"
 
     reply_markup = ReplyKeyboardMarkup(custom_keyboard)
     update.message.reply_markdown(reply_markup=reply_markup, text=text_response)
@@ -35,13 +52,22 @@ def main() -> None:
     """Start the bot."""
     load_dotenv(Path.cwd() / ".env")
     tg_api_token = os.environ["TG_API_KEY"]
+    redis_host = os.environ["REDIS_HOST"]
+    redis_port = int(os.environ["REDIS_PORT"])
+    redis_password = os.environ["REDIS_PASSWORD"]
+
+    redis_conn = redis.Redis(
+        host=redis_host,
+        port=redis_port,
+        password=redis_password)
+
     questions_files = glob.glob("./quiz-questions/*.txt")
     questions = load_questions(questions_files)
     updater = Updater(tg_api_token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(
-        MessageHandler(Filters.text & ~Filters.command, partial(reply, questions=questions))
+        MessageHandler(Filters.text & ~Filters.command, partial(reply, questions=questions, redis_conn=redis_conn))
     )
     updater.start_polling()
     updater.idle()
